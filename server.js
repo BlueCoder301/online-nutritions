aad6cca5ba213bc1efaf05fa27a42680b2f9033e
samahthwib <samah.thweib@hotmail.com>
2020-04-19 14:57:09 +0300

    home page

--- server.js
+++ server.js
@@ -18,83 +18,88 @@ const client = new pg.Client(process.env.DATABASE_URL);
 
 var request = require("request");
 
+app.get('/' , getIndex);
 
-// BMI Calculator API
-//-----------------------------------------------------------------------------------
-var options = {
-  method: 'POST',
-  url: 'https://bmi.p.rapidapi.com/',
-  headers: {
-    'x-rapidapi-host': 'bmi.p.rapidapi.com',
-    'x-rapidapi-key': '4218e6b09amsh83ad7514517c049p1976dbjsn3737c4582a4c',
-    'content-type': 'application/json',
-    accept: 'application/json'
-  },
-  body: {
-    weight: {value: '85.00', unit: 'kg'},
-    height: {value: '170.00', unit: 'cm'},
-    sex: 'm',
-    age: '24',
-    waist: '34.00',
-    hip: '40.00'
-  },
-  json: true
-};
+function getIndex(req,res){
+  res.render('pages/index');
+}
 
-request(options, function (error, response, body) {
-	if (error) throw new Error(error);
+// // BMI Calculator API
+// //-----------------------------------------------------------------------------------
+// var options = {
+//   method: 'POST',
+//   url: 'https://bmi.p.rapidapi.com/',
+//   headers: {
+//     'x-rapidapi-host': 'bmi.p.rapidapi.com',
+//     'x-rapidapi-key': '4218e6b09amsh83ad7514517c049p1976dbjsn3737c4582a4c',
+//     'content-type': 'application/json',
+//     accept: 'application/json'
+//   },
+//   body: {
+//     weight: {value: '85.00', unit: 'kg'},
+//     height: {value: '170.00', unit: 'cm'},
+//     sex: 'm',
+//     age: '24',
+//     waist: '34.00',
+//     hip: '40.00'
+//   },
+//   json: true
+// };
 
-    console.log(body);
-});
+// request(options, function (error, response, body) {
+// 	if (error) throw new Error(error);
 
-//-----------------------------------------------------------------------------------
+//     console.log(body);
+// });
 
-// Get Meal Plane 
-//-----------------------------------------------------------------------------------
+// //-----------------------------------------------------------------------------------
 
-var request = require("request");
+// // Get Meal Plane 
+// //-----------------------------------------------------------------------------------
 
-var options = {
-  method: 'GET',
-  url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate',
-  qs: {
-    timeFrame: 'day',
-    targetCalories: '2000',
-    diet: 'vegetarian',
-    exclude: 'shellfish%2C olives'
-  },
-  headers: {
-    'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-    'x-rapidapi-key': '4218e6b09amsh83ad7514517c049p1976dbjsn3737c4582a4c'
-  }
-};
+// var request = require("request");
 
-request(options, function (error, response, body) {
-	if (error) throw new Error(error);
+// var options = {
+//   method: 'GET',
+//   url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate',
+//   qs: {
+//     timeFrame: 'day',
+//     targetCalories: '2000',
+//     diet: 'vegetarian',
+//     exclude: 'shellfish%2C olives'
+//   },
+//   headers: {
+//     'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+//     'x-rapidapi-key': '4218e6b09amsh83ad7514517c049p1976dbjsn3737c4582a4c'
+//   }
+// };
 
-	console.log(body);
-});
-//-----------------------------------------------------------------------------------
-// Get calories in meal  
+// request(options, function (error, response, body) {
+// 	if (error) throw new Error(error);
 
-var request = require("request");
+// 	console.log(body);
+// });
+// //-----------------------------------------------------------------------------------
+// // Get calories in meal  
 
-var options = {
-  method: 'GET',
-  url: 'https://nutritionix-api.p.rapidapi.com/v1_1/search/cheddar%2520cheese',
-  qs: {fields: 'item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat'},
-  headers: {
-    'x-rapidapi-host': 'nutritionix-api.p.rapidapi.com',
-    'x-rapidapi-key': '4218e6b09amsh83ad7514517c049p1976dbjsn3737c4582a4c'
-  }
-};
+// var request = require("request");
 
-request(options, function (error, response, body) {
-	if (error) throw new Error(error);
+// var options = {
+//   method: 'GET',
+//   url: 'https://nutritionix-api.p.rapidapi.com/v1_1/search/cheddar%2520cheese',
+//   qs: {fields: 'item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat'},
+//   headers: {
+//     'x-rapidapi-host': 'nutritionix-api.p.rapidapi.com',
+//     'x-rapidapi-key': '4218e6b09amsh83ad7514517c049p1976dbjsn3737c4582a4c'
+//   }
+// };
 
-	console.log(body);
-});
-//-----------------------------------------------------------------------------------
+// request(options, function (error, response, body) {
+// 	if (error) throw new Error(error);
+
+// 	console.log(body);
+// });
+// //-----------------------------------------------------------------------------------
 
 
 
@@ -102,10 +107,13 @@ request(options, function (error, response, body) {
 app.get('*', (req, res) => {
     res.status(404).send('This route does not exist!!');
 })
-client.connect()
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Listening on PORT ${PORT}`)
-        })
 
-    })
+app.listen(PORT, () => {
+  console.log(`Listening on PORT ${PORT}`)
+})
+
+// client.connect()
+//     .then(() => {
+      
+
+//     })
