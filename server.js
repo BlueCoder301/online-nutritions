31276310138334186ab5b87e32c2194894aadd33
Tumaho <obadatumah19@gmail.com>
2020-04-21 13:55:39 +0300

    obada

--- server.js
+++ server.js
@@ -30,12 +30,12 @@ var request = require("request");
 
 app.get('/about-us', aboutUs);
 
-app.get('/' , getIndex);
-app.get('/info',showCalculater);
-app.get('/result',calculater);
-app.post('/result',usernameExist);
-app.post('/diet' , getMeals);
-app.post('/meals',meals);
+app.get('/', getIndex);
+app.get('/info', showCalculater);
+app.get('/result', calculater);
+app.post('/result', usernameExist);
+app.post('/diet', getMeals);
+app.post('/meals', meals);
 
 
 
@@ -45,112 +45,112 @@ app.get('/test', test);
 
 
 function getMeals(req, res) {
-    res.render('pages/meals');
+  res.render('pages/meals');
 }
 app.get('/search', searchRout);
 
 function getIndex(req, res) {
-    res.render('pages/index');
+  res.render('pages/index');
 }
 
 function showCalculater(req, res) {
-    res.render('pages/calculate');
+  res.render('pages/calculate');
 }
 
 function calculater(req, res) {
-    let userName = req.query.username;
-    let weightInfo = req.query.weight;
-    let heightInfo = req.query.height;
-    let ageInfo = req.query.age;
-    let genderInfo = req.query.gender;
-    let SQL1 = 'SELECT user_name FROM user_info;';
-    client.query(SQL1)
-        .then(data => {
-            let exist = true;
-            data.rows.forEach(val => {
-                if (val.user_name === userName) { exist = false; }
-            })
-            if (exist) {
-                let SQL = 'INSERT INTO user_info (user_name,weight,height,age,gender) VALUES ($1,$2,$3,$4,$5);';
-                let safeValues = [userName, weightInfo, heightInfo, ageInfo, genderInfo];
-                client.query(SQL, safeValues)
-                    .then(data => { console.log('done'); })
-                var informationOptions = {
-                    method: 'POST',
-                    url: 'https://bmi.p.rapidapi.com/',
-                    headers: {
-                        'x-rapidapi-host': 'bmi.p.rapidapi.com',
-                        'x-rapidapi-key': 'de72feb0d1msh4cd6880191f064fp1248d3jsnb2c8b04da480',
-                        'content-type': 'application/json',
-                        accept: 'application/json'
-                    },
-                    body: {
-                        weight: { value: weightInfo, unit: 'kg' },
-                        height: { value: heightInfo, unit: 'cm' },
-                        sex: genderInfo,
-                        age: ageInfo
-                    },
-                    json: true
-                };
-                request(informationOptions, function(error, response, body) {
-                    if (error) throw new Error(error);
-                    information = body;
-                    calories = information.bmr.value;
-                    res.render('pages/results', { info: information });
-                });
-            } else {
-                console.log('the user name already exist');
-            }
+  let userName = req.query.username;
+  let weightInfo = req.query.weight;
+  let heightInfo = req.query.height;
+  let ageInfo = req.query.age;
+  let genderInfo = req.query.gender;
+  let SQL1 = 'SELECT user_name FROM user_info;';
+  client.query(SQL1)
+    .then(data => {
+      let exist = true;
+      data.rows.forEach(val => {
+        if (val.user_name === userName) { exist = false; }
+      })
+      if (exist) {
+        let SQL = 'INSERT INTO user_info (user_name,weight,height,age,gender) VALUES ($1,$2,$3,$4,$5);';
+        let safeValues = [userName, weightInfo, heightInfo, ageInfo, genderInfo];
+        client.query(SQL, safeValues)
+          .then(data => { console.log('done'); })
+        var informationOptions = {
+          method: 'POST',
+          url: 'https://bmi.p.rapidapi.com/',
+          headers: {
+            'x-rapidapi-host': 'bmi.p.rapidapi.com',
+            'x-rapidapi-key': 'de72feb0d1msh4cd6880191f064fp1248d3jsnb2c8b04da480',
+            'content-type': 'application/json',
+            accept: 'application/json'
+          },
+          body: {
+            weight: { value: weightInfo, unit: 'kg' },
+            height: { value: heightInfo, unit: 'cm' },
+            sex: genderInfo,
+            age: ageInfo
+          },
+          json: true
+        };
+        request(informationOptions, function (error, response, body) {
+          if (error) throw new Error(error);
+          information = body;
+          calories = information.bmr.value;
+          res.render('pages/results', { info: information });
+        });
+      } else {
+        console.log('the user name already exist');
+      }
 
 
-        })
+    })
 }
 
 function usernameExist(req, res) {
-    let userNameKeyword = req.body.previousUsername;
-    let SQL = `SELECT user_name FROM user_info ;`;
-    client.query(SQL)
-        .then(data => {
-            let exist = false;
-            data.rows.forEach(val => {
-                if (val.user_name === userNameKeyword) { exist = true; }
-            })
-            if (exist) {
-                let SQL1 = `SELECT * FROM user_info WHERE user_name = '${userNameKeyword}';`;
-                client.query(SQL1)
-                    .then(dataSaved => {
-                        var informationOptions = {
-                            method: 'POST',
-                            url: 'https://bmi.p.rapidapi.com/',
-                            headers: {
-                                'x-rapidapi-host': 'bmi.p.rapidapi.com',
-                                'x-rapidapi-key': 'de72feb0d1msh4cd6880191f064fp1248d3jsnb2c8b04da480',
-                                'content-type': 'application/json',
-                                accept: 'application/json'
-                            },
-                            body: {
-                                weight: { value: dataSaved.rows[0].weight, unit: 'kg' },
-                                height: { value: dataSaved.rows[0].height, unit: 'cm' },
-                                sex: dataSaved.rows[0].gender,
-                                age: dataSaved.rows[0].age
-                            },
-                            json: true
-                        };
-                        request(informationOptions, function(error, response, body) {
-                            if (error) throw new Error(error);
-                            information = body;
-                            calories = information.bmr.value;
-                            res.render('pages/results', { info: information, DB: dataSaved.rows[0] });
+  let userNameKeyword = req.body.previousUsername;
+  let SQL = `SELECT user_name FROM user_info ;`;
+  client.query(SQL)
+    .then(data => {
+      let exist = false;
+      data.rows.forEach(val => {
+        if (val.user_name === userNameKeyword) { exist = true; }
+      })
+      if (exist) {
+        let SQL1 = `SELECT * FROM user_info WHERE user_name = '${userNameKeyword}';`;
+        client.query(SQL1)
+          .then(dataSaved => {
+            var informationOptions = {
+              method: 'POST',
+              url: 'https://bmi.p.rapidapi.com/',
+              headers: {
+                'x-rapidapi-host': 'bmi.p.rapidapi.com',
+                'x-rapidapi-key': 'de72feb0d1msh4cd6880191f064fp1248d3jsnb2c8b04da480',
+                'content-type': 'application/json',
+                accept: 'application/json'
+              },
+              body: {
+                weight: { value: dataSaved.rows[0].weight, unit: 'kg' },
+                height: { value: dataSaved.rows[0].height, unit: 'cm' },
+                sex: dataSaved.rows[0].gender,
+                age: dataSaved.rows[0].age
+              },
+              json: true
+            };
+            request(informationOptions, function (error, response, body) {
+              if (error) throw new Error(error);
+              information = body;
+              calories = information.bmr.value;
+              res.render('pages/results', { info: information, DB: dataSaved.rows[0] });
 
-                        });
+            });
 
-                    })
-            }
+          })
+      }
 
-        })
+    })
 
 }
-function meals(req,res){
+function meals(req, res) {
   let type = req.body.Vegan;
   // var mealOptions = {
   //   method: 'GET',
@@ -175,7 +175,7 @@ function meals(req,res){
       number: '10',
       random: true,
       maxCalories: calories,
-      
+
       limitLicense: 'false'
     },
     headers: {
@@ -190,49 +190,49 @@ function meals(req,res){
     mealsOutput1 = JSON.parse(body);
     res.render('pages/meals', { meal1: mealsOutput1 });
   });
-  
+
   // request(mealOptions, function (error, response, body) {
   //   if (error) throw new Error(error);
   //   mealsOutput = JSON.parse(body);
   //   console.log(mealsOutput1);
-    
+
   // });
 }
 
 
 function searchRout(req, res) {
-    let keyWord = req.query.input;
-    var options = {
-        method: 'GET',
-        url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex',
-        qs: {
-            query: keyWord,
-            minProtein: '5',
-            maxProtein: '100',
-            limitLicense: 'false',
-            offset: '0',
-            number: '10'
-        },
-        headers: {
-            'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
-            'x-rapidapi-key': 'de72feb0d1msh4cd6880191f064fp1248d3jsnb2c8b04da480'
-        }
-    };
+  let keyWord = req.query.input;
+  var options = {
+    method: 'GET',
+    url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/searchComplex',
+    qs: {
+      query: keyWord,
+      minProtein: '5',
+      maxProtein: '100',
+      limitLicense: 'false',
+      offset: '0',
+      number: '10'
+    },
+    headers: {
+      'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+      'x-rapidapi-key': 'de72feb0d1msh4cd6880191f064fp1248d3jsnb2c8b04da480'
+    }
+  };
 
-    request(options, function(error, response, body) {
-        if (error) throw new Error(error);
-        searchResults = JSON.parse(body);;
-        res.render('pages/search', { food: searchResults });
-    });
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    searchResults = JSON.parse(body);;
+    res.render('pages/search', { food: searchResults });
+  });
 }
 
 
 function aboutUs(req, res) {
-    res.render('pages/about-us')
+  res.render('pages/about-us')
 }
 
 function test(req, res) {
-    res.render('pages/test')
+  res.render('pages/test')
 }
 
 
@@ -243,15 +243,14 @@ function test(req, res) {
 
 
 app.get('*', (req, res) => {
-        res.status(404).send('This route does not exist!!');
-    })
-    // client.connect()
-    //   .then(() => {
-
-//   });
-
-
-
-app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`)
+  res.status(404).send('This route does not exist!!');
 })
+client.connect()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Listening on PORT ${PORT}`)
+    })
+  });
+
+
+
