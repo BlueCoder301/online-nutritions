b84d92b6707af893c9634ec3823aa85023b9865a
samahthwib <samah.thweib@hotmail.com>
2020-04-22 17:32:51 +0300

    kkkk

--- server.js
+++ server.js
@@ -147,9 +147,6 @@ function usernameExist(req, res) {
 
           })
       }
-      else{
-        res.render('pages/calculate', { msg: 'This user name is not exist' });
-      }
 
     })
 
@@ -179,7 +176,7 @@ function meals(req, res) {
       number: '10',
       random: true,
       maxCalories: calories,
-      
+
       limitLicense: 'false'
     },
     headers: {
